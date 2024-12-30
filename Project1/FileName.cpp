3c1e1d92739fefe24225d403a21af7048058039c
ASCII-58 <119689982+ASCII-58@users.noreply.github.com>
2024-12-30 20:15:20 +0800

    0

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
-#include <string>
-#include <windows.h>
-#include <cmath> // 包含cmath库以使用sqrt函数
 using namespace std;
 
-int main()
-{	
-	string space = " ";
-	string output = "";
-	string string1 = "Hello World";
-	for (int i = 0; i < string1.length(); i++)
-	{
-		if (string1[i] != ' ')
-		{
-			for (char j = 65; j < string1[i]; j++)
-			{
-				if (!(j >= 65 && j <= 90 || j >= 97 && j <= 122))
-				{
-					continue;
-				}
-				else
-				{
-					
-					cout << output << j << endl;
-					// 增加计算量
-					for (int k = 0; k < 4000000; k++)
-					{
-						double x = sqrt(k);
-					}
-				}
-			}
-			output += string1[i];
-		}
-		else
-		{
-			output += ' ';
-			cout << output << endl;
-		}
-	}
-	cout << string1 << endl;
+int main() {
+	cout << ("asfas" && '1') << endl; return 0;
 }
